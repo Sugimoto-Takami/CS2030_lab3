68265856b3fa6c49783309d584f98fb1daeeb9d7
takami <minminzemi.gabugabu@gmail.com>
2024-02-23 23:18:17 +0800

    ddd

--- DefaultServiceTime.java
+++ DefaultServiceTime.java
@@ -4,6 +4,7 @@ import java.util.function.Supplier;
 class DefaultServiceTime implements Supplier<Double> {
     public Double get() {
         return  Math.random();
+        
         //return 1.0;
     }
 }
